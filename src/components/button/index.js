f01aaf6d473d4fcf2311fc87f555da9a13d41d2f
jzz7280 <1563382649@qq.com>
2018-06-27 22:02:14 +0800

    update

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,16 +2,10 @@ import React, { Component } from 'react';
 import Button from './button';
 
 export default class extends Component {
-  state = {
-    value: 0
-  }
   render(){
-    console.log(this.state.value);
     return <div><Button type="submit" onClick={e=>{
-      this.setState({
-        value: 0
-      })
-    }}>123</Button>
+      console.log('click')
+    }}>button</Button>
       <Button active>active</Button>
     </div>
   }
